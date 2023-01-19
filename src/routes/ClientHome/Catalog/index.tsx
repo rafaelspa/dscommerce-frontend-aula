6e551fd6c973706227673ff473755f98abdbe840
rafaelspa <rsdepaula67@gmail.com>
2023-01-19 15:45:30 -0300

    Query params

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,26 +6,36 @@ import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 
+type QueryParams = {
+  page: number;
+  name: string;
+};
+
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-  const [productName, setProductName] = useState<string>("");
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: "",
+  });
 
   useEffect(() => {
-    productService.findPageRequest(0, productName).then((response) => {
-      setProducts(response.data.content);
-    });
-  }, [productName]);
+    productService
+      .findPageRequest(queryParams.page, queryParams.name)
+      .then((response) => {
+        setProducts(response.data.content);
+      });
+  }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setProductName(searchText)
+    setQueryParams({ ...queryParams, name: searchText });
   }
 
   return (
     <>
       <main>
         <section id="catalog-section" className="dsc-container">
-          <SearchBar onSearch={handleSearch}/>
+          <SearchBar onSearch={handleSearch} />
 
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
             {products &&
