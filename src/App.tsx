3658a1b7bd7571441c854efcad1fc413bb4f25c0
rafaelspa <rsdepaula67@gmail.com>
2023-01-19 09:15:40 -0300

    ProductDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import "./App.css";
-import HeaderClient from "./components/HeaderClient";
-import ButtonPrimary from "./components/ButtonPrimary";
-import ButtonInverse from "./components/ButtonInverse";
-import ProductDetailsCard from "./components/ProductDetailsCard";
+import ProductDetails from "./routes/ProductDetails";
 
-function App() {
+
+export default  function App() {
   return (
-    <>
-      <HeaderClient />
-      <main>
-        <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
-          <div className="dsc-btn-container">
-            <ButtonPrimary />
-            <ButtonInverse />
-          </div>
-        </section>
-      </main>
-    </>
+    <ProductDetails />
   );
 }
-
-export default App;
---
+++ src/routes/ProductDetails/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import HeaderClient from "../../components/HeaderClient";
+import ProductDetailsCard from "../../components/ProductDetailsCard";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import ButtonInverse from '../../components/ButtonInverse';
+
+export default function ProductDetails() {
+  return (
+    <>
+      <HeaderClient />
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <ProductDetailsCard />
+          <div className="dsc-btn-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
