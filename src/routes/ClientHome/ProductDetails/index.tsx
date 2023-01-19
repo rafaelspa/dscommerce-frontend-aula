265b25d636ad56531c10c9a8cde2d1ab0cb647bd
rafaelspa <rsdepaula67@gmail.com>
2023-01-19 11:04:03 -0300

    ProductDetail axios to backend

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,11 +5,21 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 export default function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/2").then((response) => {
+     const prod = response.data;
+     setProduct(prod);
+    });
+  }, []);
 
   return (
     <>
