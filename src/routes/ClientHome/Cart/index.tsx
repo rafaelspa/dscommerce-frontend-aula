e1aef6fb49a2a39a688585b77924afb9e7fbab74
rafaelspa <rsdepaula67@gmail.com>
2023-01-19 14:21:17 -0300

    Increase/decrease cart items

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -9,7 +9,17 @@ export default function Cart() {
 
   function handleClearClick() {
     cartService.clearCart();
-    setCart(cartService.getCart())
+    setCart(cartService.getCart());
+  }
+
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
+  function handleDecreaseItem(productId: number) {
+    cartService.decreaseItem(productId);
+    setCart(cartService.getCart());
   }
 
   return (
@@ -33,9 +43,20 @@ export default function Cart() {
                   <div className="dsc-cart-item-description">
                     <h3>{item.name}</h3>
                     <div className="dsc-cart-item-quantity-container">
-                      <div className="dsc-cart-item-quantity-btn">-</div>
+                      <div
+                        onClick={() => handleDecreaseItem(item.productId)}
+                        className="dsc-cart-item-quantity-btn"
+                      >
+                        -
+                      </div>
+
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div
+                        onClick={() => handleIncreaseItem(item.productId)}
+                        className="dsc-cart-item-quantity-btn"
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -56,7 +77,9 @@ export default function Cart() {
           <Link to="/catalog">
             <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
           </Link>
-          <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">Limpar carrinho</div>
+          <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">
+            Limpar carrinho
+          </div>
         </div>
       </section>
     </main>
