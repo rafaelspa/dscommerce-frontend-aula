19613c1c198c09b2dfa01ac7ed66e8d683a37d3a
rafaelspa <rsdepaula67@gmail.com>
2023-01-20 08:24:45 -0300

    Redirecting outside React component

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -15,6 +15,7 @@ export default function Login() {
       .loginRequest(formData)
       .then((response) => {
         authService.saveAccessToken(response.data.access_token);
+        
       })
       .catch((error) => {
         console.log("Erro no login", error);
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
 import * as authService from "../services/auth-service";
+import { history } from "./history";
 
 export async function requestBackend(config: AxiosRequestConfig) {
   const headers = config.withCredentials
@@ -33,10 +34,10 @@ axios.interceptors.response.use(
   },
   function (error) {
     if (error.response.status === 401) {
-      console.log("DEU 401!!");
+        history.push("/login");
     }
     if (error.response.status === 403) {
-      console.log("DEU 403!!");
+      history.push("/catalog");
     }
     return Promise.reject(error);
   }
