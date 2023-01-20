9fcf22e4da35f0bc970781b20e07ad5b84c7c0c9
rafaelspa <rsdepaula67@gmail.com>
2023-01-20 13:59:57 -0300

    Admin product search bar

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -4,6 +4,7 @@ import editIcon from "../../../assets/edit.svg";
 import deleteIcon from "../../../assets/delete.svg";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
+import SearchBar from "../../../components/SearchBar";
 
 type QueryParams = {
   page: number;
@@ -30,6 +31,11 @@ export default function ProductListing() {
       });
   }, [queryParams]);
 
+  function handleSearch(searchText: string) {
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
   return (
     <main>
       <section id="product-listing-section" className="dsc-container">
@@ -39,24 +45,22 @@ export default function ProductListing() {
           <div className="dsc-btn dsc-btn-white">Novo</div>
         </div>
 
-        <form className="dsc-search-bar">
-          <button type="submit">🔎︎</button>
-          <input type="text" placeholder="Nome do produto" />
-          <button type="reset">🗙</button>
-        </form>
+        <SearchBar onSearch={handleSearch} />
 
         <table className="dsc-table dsc-mb20 dsc-mt20">
           <thead>
-            <th className="dsc-tb576">ID</th>
-            <th></th>
-            <th className="dsc-tb768">Preço</th>
-            <th className="dsc-txt-left">Nome</th>
-            <th></th>
-            <th></th>
+            <tr>
+              <th className="dsc-tb576">ID</th>
+              <th></th>
+              <th className="dsc-tb768">Preço</th>
+              <th className="dsc-txt-left">Nome</th>
+              <th></th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
             {products.map((product) => (
-              <tr>
+              <tr key={product.id}>
                 <td className="dsc-tb576">{product.id}</td>
                 <td>
                   <img
